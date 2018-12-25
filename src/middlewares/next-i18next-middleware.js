ff13ab263ec7aa292b96e5f4df080e6456792c9a
LunaStudio_KentWood <LunaStudio_KentWood@git.cloud.tencent.com>
2018-12-25 20:40:49 +0800

    middleware support customHandler, for a continue route logic

--- src/middlewares/next-i18next-middleware.js
+++ src/middlewares/next-i18next-middleware.js
@@ -1,24 +1,32 @@
-import i18nextMiddleware from 'i18next-express-middleware'
-import { forceTrailingSlash, lngPathDetector } from 'utils'
-import { parse } from 'url'
+import i18nextMiddleware from "i18next-express-middleware";
+import { forceTrailingSlash, lngPathDetector } from "utils";
+import { parse } from "url";
 
-export default function (nexti18next, app, server) {
+export default function(nexti18next, app, server, customHandler) {
+  const { config, i18n } = nexti18next;
+  const { allLanguages, localeSubpaths } = config;
 
-  const { config, i18n } = nexti18next
-  const { allLanguages, localeSubpaths } = config
-
-  server.use(i18nextMiddleware.handle(i18n, {
-    ignoreRoutes: ['/_next', '/static'],
-  }))
+  server.use(
+    i18nextMiddleware.handle(i18n, {
+      ignoreRoutes: ["/_next", "/static"]
+    })
+  );
 
   if (localeSubpaths) {
-    server.get(/^\/(?!_next|static).*$/, forceTrailingSlash)
-    server.get(/^\/(?!_next|static).*$/, lngPathDetector)
-    server.get(`/:lng(${allLanguages.join('|')})/*`, (req, res) => {
-      const { lng } = req.params
-      const { query } = req
-      const url = parse(req.url).pathname
-      app.render(req, res, url.replace(`/${lng}`, ''), { lng, ...query })
-    })
+    server.get(/^\/(?!_next|static).*$/, forceTrailingSlash);
+    server.get(/^\/(?!_next|static).*$/, lngPathDetector);
+    server.get(`/:lng(${allLanguages.join("|")})/*`, (req, res) => {
+      const { lng } = req.params;
+      const { query } = req;
+      const url = parse(req.url).pathname;
+      if (customHandler)
+        customHandler({
+          req,
+          res,
+          route: url.replace(`/${lng}`, ""),
+          query: { lng, ...query }
+        });
+      else app.render(req, res, url.replace(`/${lng}`, ""), { lng, ...query });
+    });
   }
 }
